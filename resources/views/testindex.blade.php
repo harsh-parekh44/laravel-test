f50b7042a8fc2f6fb11386526ce350a183f393b3
vishalmaurya21-lab <vishal.kumar@saawahiitsolution.com>
2026-04-01 17:19:57 +0530

    first commit

---
+++ resources/views/testindex.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Welcome in laravel </h2>
+    <p>Laravel Prectice for git</p>
+    <h2>Into the starlignt </h2>
+</body>
+</html>
