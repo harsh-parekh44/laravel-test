1aa6fb4b9ce94c8da827c396edbf570c62b93d03
harsh-parekh44 <harsh.parekh@saawahiitsolution.com>
2026-04-02 12:27:09 +0530

    Added welcome file for harsh user

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,4 +1,5 @@
 <div>
     <!-- Well begun is half done. - Aristotle -->
     <h2>Demo Page</h2>
+    <p>Into the starlight rushing in spray </p>
 </div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+
+    <h2>Welcome harsh parekh</h2>
+</div>
