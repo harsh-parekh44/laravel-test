e2329b9f49dcd4017f7267230c912c639f395eb4
harsh-parekh44 <harsh.parekh@saawahiitsolution.com>
2026-04-07 12:05:20 +0530

    API updates with resource

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EmployeeResource;
 use Illuminate\Http\Request;
 use App\Models\Employee;
 class EmployeeController extends Controller
@@ -21,7 +22,38 @@ class EmployeeController extends Controller
     return response()->json([
         'status' => true,
         'message' => 'Employee Created Successfully.',
-        'data' => $employee
-    ], 201);
+        'data' => new EmployeeResource($employee)
+    ], 201); 
 }
+
+public function index()
+{
+    $employees = Employee::all();
+
+    return EmployeeResource::collection($employees);
+    // return response()->json([
+    //     'status' => true,
+    //     'data' => EmployeeResource::collection($employees)
+    // ]);
+}
+
+    public function show($id){
+        $employee = Employee::find($id);
+
+        if(!$employee){
+        return response()->json([
+            'status' => false,
+            'message' => 'Employee not found!'
+        ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $employee
+        ]);
+    }
+    
+    public function update($id){
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\Api\EmployeeController;
 
 
-Route::post('/employee', [EmployeeController::class, 'store'])->name('employee.store');
+Route::post('/employee', [EmployeeController::class, 'store']);
+Route::get('/employee', [EmployeeController::class, 'index']);
+
+Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+Route::post('/employee/{id}', [EmployeeController::class, 'update']);
